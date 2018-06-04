0d160a8b6753faaf52a275b35c89ca2b86701779
Aaron Sutton <aaronjsutton@icloud.com>
2018-06-04 00:46:38 -0400

    Begin typedefs and #define-s

--- src/deck.h
+++ src/deck.h
@@ -1,7 +1,50 @@
 /**
  * deck.h
- * A library for handing the construction and manipulation
+ * @file A library for handing the construction and manipulation
  * of a simulated deck of cards.
  *
  * Copyright © 2018, Aaron Sutton <aaronjsutton@icloud.com>
  */
+#ifndef DECK
+#define DECK
+
+#define DECK_SIZE   52;
+
+#define RANK_ACE    1;
+#define RANK_JACK   11;
+#define RANK_QUEEN  12;
+#define RANK_KING   13;
+
+typedef enum suit {
+  clubs = 1,
+  diamonds,
+  hearts,
+  spades
+} suit_e;
+
+/**
+  * @brief A type representing a playing card.
+  * A card can have the follwing rank:
+  * - 1       Ace
+  * - 2...10  Number value
+  * - 11      Jack
+  * - 12      Queen
+  * - 13      King
+  *
+  * A card can have the follwing suits:
+  * - Hearts
+  * - Spades
+  * - Clubs
+  * - Diamonds
+  *
+  */
+typedef struct card {
+  suit_e suit;  /**< The suit of the card. Detailed in description. */
+  int rank;     /**< Rank of the card. Detailed in description. */
+} card;
+
+typedef struct deck {
+  card *cards;
+} deck;
+
+#endif
